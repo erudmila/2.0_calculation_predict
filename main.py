b13387c2246f712735960da1ce5c0a368a7837a7
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-21 12:21:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Add comments to this code to predict what it will do.
+print(8 + 2) # will add 8 + 2 and output 10
+
+print( 8 - 2) # will subtract 8 - 2 and output 6
+
+print(8 * 2) # will multiply 8 and 2 and output 16
+
+print(10 / 4) # will divide 10/4 output ~2.5
+
+print(10 // 4) # will divide 10/4 but putput only the whole part 
+
+# This code uses variables in place of numbers in the calculation. Add comments to 
+# explain how it works.
+
+num1 = 20 # creates a variable to hold an interger = 20
+num2 = 5 #crates a variable to hold an interger = 5
+
+result = num1 * num2 #will multiply 2 intergers and it wil store the calculation in a 
+# variable called result.
+
+print(result) # it will output to the console the value store in the result variable
+
+# This code uses input to assign data into a variable.  Add comments to explain how it 
+# works.
+
+print("Enter a number") #it will output "enter a number" on the console 
+num1 = int(input()) #creates a varibale & will assign for an interger, be input by user
+num2 = 2 #it will assign the alue integet 2 to the variable num2
+
+result = num1 // num2 #the integral division of num1 and num2 will be store in a v
+#variable called result
+# if i were to enter 5 the result of the dividion would have been 2.5 but it would 
+# output the interger part of the number
+print(result) # it will out to console the value store  the result
+
+# after the code was ran i did not predict that the result will be 100 since num1 is 20 
+#and num2 is 5 when you multiply it gives you 100
